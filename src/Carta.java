4b567f408f77ebafd090df83723e63a0f8cf6eff
Gustavo Galli <gustavo.galli@elo7.com>
2021-07-30 20:32:39 -0300

    Adicionando toString a Carta

--- src/Carta.java
+++ src/Carta.java
@@ -2,6 +2,15 @@ public class Carta {
 	private String simbolo;
 	private String cor;
 	
+	public Carta(String simbolo, String cor) {
+		this.simbolo = simbolo;
+		this.cor = cor;
+	}
+	
+	public String toString() {
+		return simbolo + "-" + cor;
+	}
+	
 	public String getSimbolo() {
 		return this.simbolo;
 	}
