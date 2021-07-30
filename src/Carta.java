dc19e0143bdef8a09db6057be6b1d8f5af6bd8c3
Gustavo Galli <gustavo.galli@elo7.com>
2021-07-30 19:08:22 -0300

    Adicionando classe Carta finalizada

---
+++ src/Carta.java
@@ -0,0 +1,15 @@
+public class Carta {
+	private String simbolo;
+	private String cor;
+	
+	public String getSimbolo() {
+		return this.simbolo;
+	}
+
+	public String getCor() {
+		return this.cor;
+	}
+	
+	
+
+}
