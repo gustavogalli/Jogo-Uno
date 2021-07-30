d86aa96db3114910eca62fe0cd9f2e86b825b04b
Gustavo Galli <gustavo.galli@elo7.com>
2021-07-30 20:32:56 -0300

    Testando métodos na Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 	public static void main(String[] args) {
 		
+		Jogo jogo = new Jogo();
 		
-		
+		jogo.welcome();
+		jogo.imprimeJogadores();
+		jogo.iniciarJogo();
+		jogo.imprimirCartasHumano();
 		
 		
 	}
