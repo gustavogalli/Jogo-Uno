05e7945f5b0ba7d8ebec102bad4f6ab87c2337af
Gustavo Galli <gustavo.galli@elo7.com>
2021-07-30 20:32:01 -0300

    Adicionando métodos em Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,3 +1,53 @@
-public class Jogador {
+import java.util.ArrayList;
 
+public class Jogador {
+	
+	private String nome;
+	ArrayList<Carta> mao = new ArrayList<>();
+	
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+	
+	// Métodos
+	public void analisarVira() {
+		
+	}
+	
+	public void analisarMaos() {
+		// Ver quantas cartas cada jogador tem nas mãos
+	}
+	
+	public void comprarCarta() {
+		
+	}
+	
+	public void receberCarta(Carta recebida) {
+		
+	}
+	
+	public void passarVez() {
+		
+	}
+	
+	public void gritarUno() {
+		
+	}
+	
+	
+	
+	
+	// Getters
+	public String getNome() {
+		return nome;
+	}
+
+	public ArrayList<Carta> getMao() {
+		return mao;
+	}
+	
 }
